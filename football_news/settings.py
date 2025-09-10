dc0d2a46e3ab8ec7f2d713f339c10ce81e0551f0
jessegvu <gojessgerard@gmail.com>
2025-09-10 16:54:34 +0700

    Minor Adjustment di CSRF Trusted Origins

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jessevan-gerard-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://jessevan=gerard-footballnews.pbp.cs.ui.ac.id",
+    "https://jessevan-gerard-footballnews.pbp.cs.ui.ac.id",
 ]
 
 
